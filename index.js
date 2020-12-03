83a33a93e7a54050ddf2cabfc21f4e015abc1a6c
numselli <58607232+numselli@users.noreply.github.com>
2020-12-02 18:50:12 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,57 @@
-const fetch = require('node-fetch');
-const randomstring = require("randomstring");
+const fetch = require('node-fetch'), randomstring = require("randomstring"), fs = require("fs");
 
-// how many times you want to check a invite? 1000 is a second. 15000 is 15 seconds.
-// this could end you to be rate-limited from the discord api.
-const checkInterval = 15000
+//				      ┌───────────────────────────────────────────────────────────────────────┐
+//              │                                                                       │
+//              │                                                                       │
+//              │  You could be rate-limited or temporarily banned from discord's api.  │
+//              │                                                                       │
+//              │                                                                     	│
+//              └───────────────────────────────────────────────────────────────────────┘
 
-setInterval(() => {
-  let code = randomstring.generate(Math.floor(Math.random() * 9) + 5);
-  fetch(`https://discordapp.com/api/invite/${code}`)
-  .then((res) => res.json())
-  .then((json) => {
-      if (json.message !== 'Unknown Invite') {
-        console.log(`Found a valid invite: ${code}`)
-      } else {
-        console.log(`Unknown invite: ${code}`)
-      }
-  });
-}, checkInterval);
+
+// delay between invite checks (miliseconds 1000 miliseconds = 1 second)
+var checkInterval = 15000;
+
+// how long to to wait after being ratelimited (miliseconds 1000 miliseconds = 1 second)
+var rateLimitDelay = 6000;
+
+// max random delay to make it hrader to be ratelimited (miliseconds 1000 miliseconds = 1 second)
+var humanLikeRandomness = 3000
+
+
+checkCode();
+function checkCode(){
+	let code = randomstring.generate(Math.floor(Math.random() * 9) + 5);
+	if (code.length>=7){
+		fetch(`https://discordapp.com/api/invite/${code}`)
+		.then((res) => {
+			if (res.status !== 429){
+				res.json();
+			}else{
+				console.log(res.statusText);
+			}
+		})
+		.then((json) => {
+			if (!json){
+				setTimeout(checkCode, checkInterval + randomDelay(rateLimitDelay,rateLimitDelay));
+				return
+			}
+			if (json.message !== 'Unknown Invite') {
+				fs.readFile('invites.json', function (err, data) {
+					var json = JSON.parse(data)
+					json.push(`${json.guild.name}: https://discord.gg/${code}`)
+					fs.writeFile("invites.json", JSON.stringify(json))
+				})
+				console.log(`Found a valid invite for ${json.guild.name}: ${code}`);
+			}else {
+				console.log(`Unknown invite: ${code}`);
+			}
+			setTimeout(checkCode, checkInterval + randomDelay(1, humanLikeRandomness));
+		});
+	}
+}
+function randomDelay(min, max){
+	var delay =  Math.random() * (max - min) + min;
+	console.log(`next check in: ${checkInterval+delay} MS`);
+	return delay
+}
